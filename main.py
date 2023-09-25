24fca60cafd291d94140dc53849163d81613a590
brindhakanchana <144699153+brindhakanchana@users.noreply.github.com>
2023-09-25 15:17:07 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,23 +1,15 @@
-#1.1 implement a recursive function to calculate the factorial of given number.
+def linearSearchProduct(productList,targetproduct):
+  indices = []
 
-"""
-1!=1×1
-2!=2×1!
-3!=3×2!
-.
-.
-10!=10×9
+  for index, product in enumerate(productList):
+    if product == targetproduct:
+      indices.append(index)
 
-"""
+  return indices
 
 
-def fact_rec(n):
-  if n==0 or n==1:
-    return 1
-  else:
-    return n*fact_rec(n-1)
-
-number=4
-res = fact_rec(number)
-
-print("the factorial of {} is {}.".format(number,res))
+#Example usage
+products = ["shoes","boot","loafer","shoes","sandal","shoes"]
+target = "shoes"
+result = linearSearchProduct(products, target)
+print(result)
